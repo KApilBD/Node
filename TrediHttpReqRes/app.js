8c6e62d4a19281ec75027ea85d85442dd37d5724
KApilBD <kapilbaraskar91@gmail.com>
2019-12-03 22:24:50 +0530

    Assignment

--- TrediHttpReqRes/app.js
+++ TrediHttpReqRes/app.js
@@ -2,13 +2,22 @@ const express = require('express');
 
 const app = express();
 
-app.use('/add-product',(req,res,next)=>{
-    console.log("Inside route add product Middleware");
-    res.send('<h1>Add Product!!!!!</h1>')
+// app.use((req,res,next) =>{
+//     console.log('first middleware');
+//     next();
+// })
+// app.use((req,res,next)=>{
+//     console.log('second middleware');
+//     res.send("<p>Assignment</p>");
+// })
+
+app.use('/users',(req,res,next)=>{
+    console.log('User middleware');
+    res.send('<p>Middleware to handle /users</p>')
 });
-app.use('/',(req,res,next)=>{
-    console.log("Inside Middleware");
-    res.send('<h1>Hello from Express!!!!!</h1>')
-});
+app.use('/', (req,res,next)=>{
+    console.log('/ middleware');
+    res.send('<p>middleware to handle /</p>')
+})
 
 app.listen(3000, () => { console.log("Server is running, press ctrl+c to stop...") })
