286375b4b76d07e7133a0028fe6391572066c899
KApilBD <kapilbaraskar91@gmail.com>
2019-12-08 18:35:40 +0530

    Added Controller

--- mvcShop/routes/shop.js
+++ mvcShop/routes/shop.js
@@ -2,21 +2,9 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
-
 const router = express.Router();
+const ProductController = require('../controllers/products');
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
-  });
-});
+router.get('/', ProductController.getProducts);
 
 module.exports = router;
