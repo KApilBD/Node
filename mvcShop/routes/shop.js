ab2071cc3853080c8d32250d52f9590aa3639f51
KApilBD <kapilbaraskar91@gmail.com>
2019-12-08 21:07:45 +0530

    Enhancments added features

--- mvcShop/routes/shop.js
+++ mvcShop/routes/shop.js
@@ -3,8 +3,12 @@ const path = require('path');
 const express = require('express');
 
 const router = express.Router();
-const ProductController = require('../controllers/products');
+const shopController = require('../controllers/shop');
 
-router.get('/', ProductController.getProducts);
+router.get('/', shopController.getIndex);
 
+router.get('/products', shopController.getProducts);
+router.get('/cart', shopController.getCart);
+router.get('/checkout', shopController.getCheckout);
+router.get('/orders', shopController.getOrders);
 module.exports = router;
