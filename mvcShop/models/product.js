45cbd956f66ff649c8b8cef99c4f4c4279768204
KApilBD <kapilbaraskar91@gmail.com>
2019-12-11 23:14:27 +0530

    Update + dyn param

--- mvcShop/models/product.js
+++ mvcShop/models/product.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
-    
+
 
 const getProductsFromFile = (cb) => {
     fs.readFile(p, (err, fileContent) => {
-        if(err){
+        if (err) {
             return cb([]);
-        }else{
+        } else {
             cb(JSON.parse(fileContent));
         }
     })
@@ -18,9 +18,10 @@ module.exports = class Product {
         this.title = title;
         this.imageUrl = imageUrl;
         this.description = description;
-        this.price = price
+        this.price = price;
     }
     save() {
+        this.id = Math.random().toString();
         getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
@@ -30,5 +31,11 @@ module.exports = class Product {
     }
     static fetchAll(cb) {
         getProductsFromFile(cb);
+    }
+    static findById(id, cb) {
+        getProductsFromFile(products => {
+            const product = products.find(p => p.id === id);
+            cb(product);
+        })
     }
 };
--- mvcShop/routes/shop.js
+++ mvcShop/routes/shop.js
@@ -8,6 +8,7 @@ const shopController = require('../controllers/shop');
 router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
+router.get('/products/:prodId', shopController.getProduct);
 router.get('/cart', shopController.getCart);
 router.get('/checkout', shopController.getCheckout);
 router.get('/orders', shopController.getOrders);
