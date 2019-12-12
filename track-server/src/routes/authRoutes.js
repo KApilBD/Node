68f617ca41398e20fdf08b47645347df6dc90d8c
KApilBD <kapilbaraskar91@gmail.com>
2019-12-12 23:36:01 +0530

    Sign In feature

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -19,4 +19,23 @@ router.post('/signup', async (req, res) => {
 
 });
 
+router.post('/singin', async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(422).send({ error: "Must provide email and password..." });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+        return res.status(422).send({ error: "Invalid Password or email..." });
+    }
+    try {
+        await user.comparePassword(password);
+        const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
+        res.send({ token });
+    } catch (err) {
+        return res.status(422).send({ error: "Invalid Password or email..." })
+    }
+
+})
+
 module.exports = router;
