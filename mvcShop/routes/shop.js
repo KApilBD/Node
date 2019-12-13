b9106f552cbfdd1178efb14333bb9005a64abe1d
KApilBD <kapilbaraskar91@gmail.com>
2019-12-13 23:43:29 +0530

    Adding Ui and Routing

--- mvcShop/routes/shop.js
+++ mvcShop/routes/shop.js
@@ -10,6 +10,7 @@ router.get('/', shopController.getIndex);
 router.get('/products', shopController.getProducts);
 router.get('/products/:prodId', shopController.getProduct);
 router.get('/cart', shopController.getCart);
+router.post('/cart', shopController.postCart);
 router.get('/checkout', shopController.getCheckout);
 router.get('/orders', shopController.getOrders);
 module.exports = router;
