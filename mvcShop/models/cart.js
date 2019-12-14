587413d452b53dfef651fd213343231fc899da41
KApilBD <kapilbaraskar91@gmail.com>
2019-12-14 23:28:42 +0530

    Cart feature added

---
+++ mvcShop/models/cart.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'cart.json'
+);
+
+module.exports = class Cart {
+    static addProduct(id, productPrice) {
+
+
+
+        fs.readFile(p, (err, fileContent) => {
+            let cart = { products: [], totalPrice: 0 }
+            if (!err) {
+                cart = JSON.parse(fileContent);
+            }
+            console.log("productId ", id, productPrice, cart)
+            const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
+            const existingProduct = cart.products[existingProductIndex];
+            let updateProduct;
+            if (existingProduct) {
+                updateProduct = { ...existingProduct };
+                updateProduct.qty = updateProduct.qty + 1;
+                cart.products = [...cart.products];
+                cart.products[existingProductIndex] = updateProduct;
+            } else {
+                updateProduct = { id: id, qty: 1 };
+                cart.products = [...cart.products, updateProduct]
+            }
+            cart.totalPrice = cart.totalPrice + +productPrice;
+            fs.writeFile(p, JSON.stringify(cart), err => {
+                console.log(err);
+            })
+        })
+    }
+}
