baed7e30aead6554ca835c8fa857c2570c9ddac3
KApilBD <kapilbaraskar91@gmail.com>
2019-12-18 22:49:01 +0530

    Cart feature added

--- mvcShop/controllers/shop.js
+++ mvcShop/controllers/shop.js
@@ -29,18 +29,38 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-    res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart'
+    Cart.getCart(cart => {
+        Product.fetchAll(products => {
+            const cartProducts = [];
+            for (product of products) {
+                const cartProductData = cart.products.find(prod => prod.id === product.id);
+                if (cartProductData) {
+                    cartProducts.push({ productData: product, qty: cartProductData.qty });
+                }
+            }
+            res.render('shop/cart', {
+                path: '/cart',
+                pageTitle: 'Your Cart',
+                products: cartProducts
+            });
+        })
     });
 };
 
 exports.postCart = (req, res, next) => {
-    const productId  = req.body.productId;
-    Product.findById(productId , (product)=>{
+    const productId = req.body.productId;
+    Product.findById(productId, (product) => {
         Cart.addProduct(productId, product.price);
     });
     res.redirect('/cart')
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+    const productId = req.body.productId;
+    Product.findById(productId, product => {
+        Cart.deleteProduct(productId, product.price);
+        res.redirect('/cart')
+    })
 };
 
 exports.getOrders = (req, res, next) => {
