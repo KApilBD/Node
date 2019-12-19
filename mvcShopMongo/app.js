b33fa39cfc34c33d6be9b2656f407532e17d06ba
KApilBD <kapilbaraskar91@gmail.com>
2019-12-19 23:14:06 +0530

    Mongo Set up and initial connection

--- mvcShopMongo/app.js
+++ mvcShopMongo/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database').mongoConnect;
 
 const app = express();
 
@@ -16,9 +17,18 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+//
+//  
+next();
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+
+mongoConnect(()=>{
+    app.listen(3000, ()=> console.log("Server is up on 3000!!!"));
+})
--- mvcShopMongo/models/product.js
+++ mvcShopMongo/models/product.js
@@ -1,65 +1,43 @@
-const fs = require('fs');
-const path = require('path');
-const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
-
-const Cart = require('./cart');
-
-
-const getProductsFromFile = (cb) => {
-    fs.readFile(p, (err, fileContent) => {
-        if (err) {
-            return cb([]);
-        } else {
-            cb(JSON.parse(fileContent));
-        }
-    })
-};
+const getDb = require('../util/database').getDb;
 
 module.exports = class Product {
-    constructor(id, title, imageUrl, description, price) {
-        this.id = id;
+    constructor(title, imageUrl, description, price) {
+        // this.id = id;
         this.title = title;
         this.imageUrl = imageUrl;
         this.description = description;
         this.price = price;
     }
     save() {
-        getProductsFromFile(products => {
-            if (this.id) {
-                const existingProductIndex = products.findIndex(prod => prod.id === this.id);
-                const updatedProducts = [...products];
-                updatedProducts[existingProductIndex] = this;
-                fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-                    console.log(err)
-                });
-            } else {
-                this.id = Math.random().toString();
-                products.push(this);
-                fs.writeFile(p, JSON.stringify(products), (err) => {
-                    console.log(err)
-                });
-            }
-        });
-    }
-    static fetchAll(cb) {
-        getProductsFromFile(cb);
-    }
-    static findById(id, cb) {
-        getProductsFromFile(products => {
-            const product = products.find(p => p.id === id);
-            cb(product);
-        })
-    }
-    static deleteById(id) {
-        getProductsFromFile(products => {
-            const product = products.find(prod => prod.id === id);
-            const updatedProducts = products.filter(p => p.id !== id);
-            fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-                if (!err) {
-                    Cart.deleteProduct(id, product.price);
-                }
+        const db = getDb();
+        return db.collection('products')
+            .insertOne(this)
+            .then(result => {
+                console.log("Result", result);  
+            })
+            .catch(err => {
+                console.log(err)
             });
-        });
-
     }
+    // static fetchAll(cb) {
+    //     getProductsFromFile(cb);
+    // }
+    // static findById(id, cb) {
+    //     getProductsFromFile(products => {
+    //         const product = products.find(p => p.id === id);
+    //         cb(product);
+    //     })
+    // }
+    // static deleteById(id) {
+    //     getProductsFromFile(products => {
+    //         const product = products.find(prod => prod.id === id);
+    //         const updatedProducts = products.filter(p => p.id !== id);
+    //         fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+    //             if (!err) {
+    //                 Cart.deleteProduct(id, product.price);
+    //             }
+    //         });
+    //     });
+
+    // }
 };
