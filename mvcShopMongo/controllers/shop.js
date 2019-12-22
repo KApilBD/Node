04c1426197a1bcd72615d1759176c27b13d8e9b0
KApilBD <kapilbaraskar91@gmail.com>
2019-12-22 18:01:08 +0530

    Fetch all

--- mvcShopMongo/controllers/shop.js
+++ mvcShopMongo/controllers/shop.js
@@ -2,12 +2,16 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll((products) => {
+    Product.fetchAll()
+    .then((products) => {
         res.render('shop/product-list', {
             prods: products,
             pageTitle: 'All Products',
             path: '/products',
         });
+    })
+    .catch(err => {
+        console.log(err);
     });
 };
 
@@ -19,12 +23,16 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-    Product.fetchAll((products) => {
+    Product.fetchAll()
+    .then((products) => {
         res.render('shop/index', {
             prods: products,
             pageTitle: 'Shop',
             path: '/',
         });
+    })
+    .catch(err => {
+        console.log(err);
     });
 };
 
