c24c598ff4c4e3f1eefa575a15f8a4d8f2e6fcf5
KApilBD <kapilbaraskar91@gmail.com>
2019-12-25 22:39:33 +0530

    Deleteing and showing the orders

--- mvcShopMongo/models/user.js
+++ mvcShopMongo/models/user.js
@@ -70,6 +70,40 @@ class User {
 
     }
 
+    addOrder() {
+        const db = getDb();
+
+        return this.getCart().then(products => {
+            const order = {
+                items: products,
+                user: {
+                    _id: new ObjectId(this._id),
+                    name: this.name
+                }
+            }
+            return db.collection('orders')
+                .insertOne(order)
+        })
+            .then(result => {
+                this.cart = { items: [] };
+                return db.collection('users').updateOne(
+                    { _id: new ObjectId(this._id) },
+                    { $set: { cart: { items: [] } } }
+                );
+            })
+            .catch(err => console.log(err));
+    }
+
+    getOrders() {
+        const db = getDb();
+
+        return db.collection('orders')
+        .find({ 'user._id': new ObjectId(this._id) })
+        .toArray()
+        .then()
+        .catch(err => console.log(err));
+    }
+
     static findById(userId) {
         const db = getDb();
         return db.collection('users')
