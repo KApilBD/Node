ff6b5759af89d6a33c0e9f280e06c68cc6d83f44
KApilBD <kapilbaraskar91@gmail.com>
2019-12-25 21:44:50 +0530

    Delete item from cart

--- mvcShopMongo/routes/shop.js
+++ mvcShopMongo/routes/shop.js
@@ -11,7 +11,7 @@ router.get('/products', shopController.getProducts);
 router.get('/products/:prodId', shopController.getProduct);
 router.get('/cart', shopController.getCart);
 router.post('/cart', shopController.postCart);
-// router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 // router.get('/checkout', shopController.getCheckout);
 // router.get('/orders', shopController.getOrders);
 
