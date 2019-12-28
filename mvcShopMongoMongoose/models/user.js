cf275d13ead67e6365521e969adc306f0a491be1
KApilBD <kapilbaraskar91@gmail.com>
2019-12-28 14:07:10 +0530

    Showing and Deleting Cart Items

--- mvcShopMongoMongoose/models/user.js
+++ mvcShopMongoMongoose/models/user.js
@@ -46,6 +46,14 @@ userSchema.methods.addToCart = function (product) {
     return this.save();
 }
 
+userSchema.methods.removeFromCart = function (productId) {
+    const updatedCartItems = this.cart.items.filter(item => {
+        return item.productId.toString() !== productId.toString();
+    });
+    this.cart.items = updatedCartItems;
+    return this.save();
+}
+
 module.exports = mongoose.model('User', userSchema);
 
 // class User {
@@ -60,47 +68,6 @@ module.exports = mongoose.model('User', userSchema);
 //         const db = getDb();
 //         return db.collection('users')
 //             .insertOne(this);
-//     }
-
-//     addToCart(product) {
-//         const db = getDb();
-//         const cartProductIndex = this.cart.items.findIndex(cp => {
-//             return cp.productId.toString() === product._id.toString();
-//         });
-//         let newQuantity = 1;
-//         const updatedCartItems = [...this.cart.items];
-
-//         if (cartProductIndex >= 0) {
-//             newQuantity = this.cart.items[cartProductIndex].quantity + 1;
-//             updatedCartItems[cartProductIndex].quantity = newQuantity;
-//         } else {
-//             updatedCartItems.push({ productId: new ObjectId(product._id), quantity: newQuantity });
-//         }
-
-//         const updatedCart = { items: updatedCartItems }
-//         return db.collection('users').updateOne(
-//             { _id: new ObjectId(this._id) },
-//             { $set: { cart: updatedCart } }
-//         );
-//     }
-
-//     getCart() {
-//         const db = getDb();
-//         const productIds = this.cart.items.map(i => i.productId);
-//         return db.collection('products')
-//             .find({ _id: { $in: productIds } })
-//             .toArray()
-//             .then(products => {
-//                 return products.map(p => {
-//                     return {
-//                         ...p,
-//                         quantity: this.cart.items.find(i => {
-//                             return i.productId.toString() === p._id.toString();
-//                         }).quantity,
-//                     }
-//                 })
-//             })
-//             .catch(err => console.log(err));
 //     }
 
 //     deleteItemFromCart(productId) {
