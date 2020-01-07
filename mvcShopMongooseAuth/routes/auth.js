ac4874f88ee850f79acef9fdbb431726512e94eb
KApilBD <kapilbaraskar91@gmail.com>
2020-01-07 23:10:03 +0530

    Signup Setup

--- mvcShopMongooseAuth/routes/auth.js
+++ mvcShopMongooseAuth/routes/auth.js
@@ -7,5 +7,7 @@ const authController = require('../controllers/auth');
 route.get('/login', authController.getLogin)
 route.post('/login', authController.postLogin)
 route.post('/logout', authController.postLogout)
+route.get('/signup', authController.getSignup);
+route.post('/signup', authController.postSignup);
 
 module.exports = route;
