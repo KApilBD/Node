dffd91c84140359e66a7e727a5854c2f9ab559ce
KApilBD <kapilbaraskar91@gmail.com>
2020-01-29 23:58:55 +0530

    Download file

--- mvcShopMongooseAuth/controllers/shop.js
+++ mvcShopMongooseAuth/controllers/shop.js
@@ -1,5 +1,7 @@
 const Product = require('../models/product');
 const Order = require('../models/order');
+const fs = require('fs');
+const path = require('path');
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -116,3 +118,17 @@ exports.getOrders = (req, res, next) => {
     })
     .catch(err => console.log(err));
 };
+
+exports.getInvoice = (req, res, next) => {
+  const orderId = req.params.orderId;
+  const invoiceName = 'invoice-' + orderId + '.pdf';
+  const invoicePath = path.join('data', 'invoices', invoiceName);
+  fs.readFile(invoicePath, (err, data) => {
+    if (err) {
+      return next(err);
+    }
+    res.setHeader('Content-Type', 'application/pdf')
+    res.setHeader('Content-Disposition', 'attachment; filename="' + invoiceName + '"');
+    res.send(data);
+  })
+}
