b8bd588325c5e6b36d8291b0d25209de5c837984
KApilBD <kapilbaraskar91@gmail.com>
2020-03-09 15:57:46 +0530

    Rest Api

---
+++ REST/rest-api-project/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const feedRoutes = require("./routes/feed");
+
+app.use(bodyParser.json()); // application/json
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods",'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+})
+
+app.use('/feed',feedRoutes);
+
+app.listen(8080, () => console.log("Running on 8080!!!"))
