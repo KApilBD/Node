47b365d76f12d9da2d0689d8b41bc9800308b885
KApilBD <kapilbaraskar91@gmail.com>
2020-03-10 23:15:16 +0530

    Configuring POST posts api, added mongoose and other configuration

--- REST/Rest-Blog/blog-BE/controllers/feed.js
+++ REST/Rest-Blog/blog-BE/controllers/feed.js
@@ -1,12 +1,15 @@
+const { validationResult } = require('express-validator');
+
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
         posts: [
             {
-                _id:"1",
+                _id: "1",
                 title: "First Post",
                 content: "this is first post",
                 imageUrl: "images/htaht.jpg",
-                creator:{
+                creator: {
                     name: "Kapil"
                 },
                 createdAt: new Date()
@@ -16,11 +19,24 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPosts = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            message: "Validation failed, entered data is incorrect.",
+            errors: errors.array()
+        })
+    }
     const title = req.body.title;
     const content = req.body.content;
     //create post in DB
     res.status(201).json({
         message: "Post created successfully!",
-        post: { id: new Date().toISOString(), title: title, content: content }
+        post: {
+            _id: new Date().toISOString(),
+            title: title,
+            content: content,
+            creator: {},
+            createdAt: new Date()
+        }
     });
 };
--- REST/Rest-Blog/blog-BE/routes/feed.js
+++ REST/Rest-Blog/blog-BE/routes/feed.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const { body } = require('express-validator');
 const feedController = require("../controllers/feed");
 const router = express.Router();
 
 router.get("/posts", feedController.getPosts);
-router.post("/post", feedController.createPosts);
+router.post("/post", [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 })
+], feedController.createPosts);
 
 module.exports = router;
