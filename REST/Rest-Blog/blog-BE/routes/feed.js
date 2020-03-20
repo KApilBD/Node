e6e81926bbd515d7e7c4cd785e4f34d03e7fff20
KApilBD <kapilbaraskar91@gmail.com>
2020-03-20 23:26:35 +0530

    Adding update and delete feature

--- REST/Rest-Blog/blog-BE/routes/feed.js
+++ REST/Rest-Blog/blog-BE/routes/feed.js
@@ -10,5 +10,12 @@ router.post("/post", [
 ], feedController.createPosts);
 
 router.get("/post/:postId", feedController.getPost);
+router.put("/post/:postId",[
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 })
+], feedController.updatePost)
+
+router.delete("/post/:postId", feedController.deletePost);
+
 
 module.exports = router;
